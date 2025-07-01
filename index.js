54031070dd4662fb5ffb488567952a0e47c70cb6
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2025-06-30 21:36:12 -0400

    Add image loading logic and create image directory variables

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var questionNum = 0;
-var score = 0;
-var isAIGenerated;
+const aiImagesLen = 0; 
+const realImagesLen = 0;
+
+let questionNum = 0;
+let score = 0;
+let isAIGenerated;
 
 function loadQuestion() {
     questionNum++;
@@ -8,10 +11,22 @@ function loadQuestion() {
     document.getElementById("score").textContent = "Score: " + score;
 
     isAIGenerated = (Math.random() < 0.5); //temporary
+    let imageDir, imgDirLen, imageFile;
+    if (isAIGenerated) {
+        imageDir = "imgs/AI/";
+        imgDirLen = aiImagesLen;
+    } else {
+        imageDir = "imgs/real/";
+        imgDirLen = realImagesLen;
+    }
+    
+    // Pick a random image from the list
+    imageFile = "img" + Math.floor(Math.random() * imgDirLen);
+    document.getElementById("quiz-img").src = imageDir + imageFile + ".jpg";
 }
 
 document.getElementById("confirm-button").onclick = function() {
-    var answer;
+    let answer;
     if (document.getElementById("yes-button").checked) {
         answer = true;
     } else if (document.getElementById("no-button").checked) {
