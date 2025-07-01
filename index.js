e9c3a50002cc3238fb6427338eece7bc215404ad
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2025-06-30 20:48:46 -0400

    Implement question loading and scoring logic for quiz

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hello world!");
+var questionNum = 0;
+var score = 0;
+var isAIGenerated;
+
+function loadQuestion() {
+    questionNum++;
+    document.getElementById("question-num").textContent = "Question #" + questionNum;
+
+    isAIGenerated = Boolean(Math.random()); //temporary
+}
+
+function answerQuestion() {
+    var answer;
+    if (document.getElementById("yes-button").checked) {
+        answer = true;
+    } else if (document.getElementById("no-button").checked) {
+        answer = false;
+    } else {
+        console.error("No button checked!!");
+        return;
+    }
+
+    if (answer == isAIGenerated) {
+        score++;
+    } else {
+        score--;
+    }
+    loadQuestion();
+}
+
+loadQuestion();
