41dfad78df541d0d46bda2e5bbf3dea0bef9cc5b
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2025-07-14 17:06:31 -0400

    Add backend code

---
+++ backend/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, send_from_directory
+import os
+import random
+
+app = Flask(__name__)
+
+ai_images_path = r'L:\datatest\ai-generated-images-vs-real-images\AiArtData\AiArtData' #these are the paths for me. Data from https://www.kaggle.com/datasets/birdy654/cifake-real-and-ai-generated-synthetic-images
+real_images_path = r'L:\datatest\ai-generated-images-vs-real-images\RealArt\RealArt'
+
+def get_random_image():
+    is_ai = random.choice([True, False])
+    folder = ai_images_path if is_ai else real_images_path
+    images = os.listdir(folder)
+    image = random.choice(images)
+    return {
+        'image_path': image,
+        'label': 'AI' if is_ai else 'Real',
+    }
+
+@app.route('/random-image', methods=['GET'])
+def random_image():
+    image = get_random_image()
+    return jsonify(image)
+
+@app.route('/images/<path:filename>', methods=['GET'])
+def get_image(filename):
+    if filename in os.listdir(ai_images_path):
+        return send_from_directory(ai_images_path, filename)
+    elif filename in os.listdir(real_images_path):
+        return send_from_directory(real_images_path, filename)
+    return "Image not found", 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
